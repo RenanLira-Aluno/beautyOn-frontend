9c6b305b5567258a4e04b4143402b94b7bd1d3b0
RenanLira-Aluno <catce.20191ads0401@aluno.ifpi.edu.com.br>
2023-12-06 22:02:42 -0300

    signup screen data reducer

--- src/screens/SignUpScreen/index.tsx
+++ src/screens/SignUpScreen/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useState } from "react"
+import { useCallback, useContext, useReducer, useState } from "react"
 import { View, Text } from "react-native"
 import { AuthContext } from "../../contexts/Auth.context"
 import { AuthSignupRequest } from "../../services/contracts/interfaces"
@@ -7,8 +7,9 @@ import { AuthSignupRequest } from "../../services/contracts/interfaces"
 export const SignUpScreen = () => {
     const {signup} = useContext(AuthContext)
 
-    const [data, setData] = useState<AuthSignupRequest>({} as AuthSignupRequest)
-
+    const [data, setData] = useReducer((state : AuthSignupRequest, newState : Partial<AuthSignupRequest>) => 
+        ({...state, ...newState}), {} as AuthSignupRequest)
+        // exemplo de uso: onChangeText={(text) => setData({email: text})}
 
     const handleButtonSignUp = useCallback(async () => {
         await signup({
